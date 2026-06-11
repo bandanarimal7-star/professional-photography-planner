bb39da17441366072ebc595d5f770cf4cd502954
govindakhattri321 <govindakhattri321@gmail.com>
2026-06-11 22:06:22 +1000

    Add 404 error handling for deployment testing

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ def home():
 @app.route("/hello")
 def hello():
     return "Hello World"
+    @app.errorhandler(404)
+def page_not_found(error):
+    return "Page not found", 404
 
 if __name__ == "__main__":
     app.run()
